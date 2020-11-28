77670d47a9fa9636e67ca3ab2d8b8da5380a5f22
VanillaTiger <a.szummer@gmail.com>
2020-11-28 02:30:54 +0100

    normalized coordinates

--- extractor.py
+++ extractor.py
@@ -2,13 +2,27 @@ import cv2
 import numpy as np
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
 
+#turn [[x,y]] -> [[x,y,1]]
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0],1))], axis=1)
 
 class Extractor(object):
-    def __init__(self):
+    def __init__(self, K):
         self.orb = cv2.ORB_create()
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last = None
+        self.K=K
+        self.Kinv=np.linalg.inv(self.K)
+
+    def normalize(self,pts):
+        return np.dot(self.Kinv, add_ones(pts).T).T[:,0:2]
+
+    def denormalize(self,pt):
+        ret = np.dot(self.K, np.array([pt[0], pt[1],1.0]))
+        # ret /= ret[2]
+        return int(round(ret[0])), int(round(ret[1]))
 
     def extract(self, frame):
         # detection of features
@@ -28,15 +42,27 @@ class Extractor(object):
                     kp2 = self.last['kps'][m.trainIdx].pt
                     ret.append((kp1, kp2))
 
+
+
+
         # filter using Fundamental matrix that provides inliers between frames
         if len(ret) > 0:
             ret = np.array(ret)
+
+            # normalize coords: substract to move to 0
+            ret[:,0,:] = self.normalize(ret[:,0,:])
+            ret[:, 1, :] = self.normalize(ret[:,1,:])
+
+            # same as above -> ret[:, :, 0] -= frame.shape[0] //2
+            # ret[:, :, 1] -= frame.shape[1] //2
+
             model, inliers = ransac((ret[:, 0], ret[:, 1]),
                                     FundamentalMatrixTransform,
                                     min_samples=8,
                                     residual_threshold=0.5,
                                     max_trials=100)
             ret = ret[inliers]
+
 
         # return
         self.last = {'kps': kps, 'des': des}
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import cv2
 from extractor import Extractor
+import numpy as np
 
+H = 480
+W = 854
+F=1
+K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
 
 def print_hi(name):
     print(f'Hi, {name}')
 
 
-fe = Extractor()
-
+fe = Extractor(K)
 
 def play_video(video):
     cap = cv2.VideoCapture(video)
@@ -15,12 +19,12 @@ def play_video(video):
     while cap.isOpened():
         ret, frame = cap.read()
         matches = fe.extract(frame)
-
         print(len(matches)," matches")
 
+
         for pt1, pt2 in matches:
-            u1, v1 = map(lambda x: int(round(x)), pt1)
-            u2, v2 = map(lambda x: int(round(x)), pt2)
+            u1, v1 = fe.denormalize(pt1)
+            u2, v2 = fe.denormalize(pt2)
             cv2.circle(frame, (u1, v1), color=(0, 255, 0), radius=2)
             cv2.line(frame, (u1, v1), (u2, v2), color=(255, 0, 0))
 
